d48dd330086039e44270bbe0a6b69a26a7099479
Tressie Muthuri <tressiemuthuri@gmail.com>
2024-05-10 21:24:30 +0300

    chore: add component files in pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import React from 'react';
 import Calculator from './components/pages/calculator/Calculator';
+import Login from './components/pages/login/Login';
+import News from './components/pages/news/News';
+import Signin from './components/pages/signin/Signin';
+import Upload from './components/pages/upload/Upload';
 
 function App() {
   return (
     <div className='App'>
       <Calculator />
+      <Login />
+      <News />
+      <Signin />
+      <Upload />
     </div>
   );
 }
