fc0e1c5870129077eaf190194fd2e13e4eeec8dc
Tressie Muthuri <tressiemuthuri@gmail.com>
2024-05-15 17:19:08 +0300

    chore: add hocs folder and redux folders

---
+++ src/hocs/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../components/molecules/navbar/Navbar'
+
+// logic:
+// when a client refreshes the app they remain logged in
+// function checks if the client is authorized
+
+const Layout = (props) => {
+  return (
+    <div>
+        <Navbar />
+        {props.children}
+    </div>
+  )
+}
+
+export default Layout
