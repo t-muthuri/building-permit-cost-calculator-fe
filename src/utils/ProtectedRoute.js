69bf159e0af4e067165de8f7b3b4ababf73573ed
Tressie Muthuri <tressiemuthuri@gmail.com>
2024-06-03 22:54:16 +0300

    fix: rerendering infinite times

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Navigate, useLocation } from 'react-router-dom';
 
-const ProtectedRoute = ({ children, isAuthenticated }) => {
-  // const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  let location = useLocation();
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const location = useLocation();
 
   if (!isAuthenticated) {
     return <Navigate to='/login' state={{ from: location }} replace />;
@@ -14,15 +14,7 @@ const ProtectedRoute = ({ children, isAuthenticated }) => {
 };
 
 ProtectedRoute.propTypes = {
-  // login: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
-  isAuthenticated: PropTypes.bool,
 };
-// ProtectedRoute
 
-//   const mapStateToProps = (state) => ({
-//     isAuthenticated: state.auth.isAuthenticated,
-//   });
-
-// export default connect(mapStateToProps, {login, isAuthenticated})(ProtectedRoute)
 export default ProtectedRoute;
