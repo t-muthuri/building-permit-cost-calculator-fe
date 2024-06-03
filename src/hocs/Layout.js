a40cca4902d26a401f42ff6493068ac3db3b5884
Tressie Muthuri <tressiemuthuri@gmail.com>
2024-06-03 23:10:13 +0300

    fix: prop validation

--- src/hocs/Layout.js
+++ src/hocs/Layout.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Navbar from '../components/molecules/navbar/Navbar';
 import { connect } from 'react-redux';
 import { checkAuthenticated, load_user } from '../modules/actions/auth';
+import PropTypes from 'prop-types';
 // logic:
 // when a client refreshes the app they remain logged in
 // function checks if the client is authorized
@@ -17,6 +18,12 @@ const Layout = (props) => {
       {props.children}
     </div>
   );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+  checkAuthenticated: PropTypes.func.isRequired,
+  load_user: PropTypes.func.isRequired,
 };
 
 export default connect(null, { checkAuthenticated, load_user })(Layout);
