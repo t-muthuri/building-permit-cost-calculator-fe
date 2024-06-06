9974e2d42812e39a20b58c05383e3c9cde290d2e
Tressie Muthuri <tressiemuthuri@gmail.com>
2024-06-06 10:39:29 +0300

    chore: ui to upload files

--- src/components/pages/upload/Upload.js
+++ src/components/pages/upload/Upload.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 const Upload = () => {
-  return <div>Upload</div>;
+  return (
+    <div>
+      <form>
+        <label htmlFor='file'>Browse a file to upload:</label>
+        <input name='file' id='file' type='file' placeholder='Upload a file' />
+        <br />
+        <input type='submit' name='submit' id='submit' />
+      </form>
+      <h1>Uploaded files</h1>
+      <table>
+        <tr>
+          <th>File name</th>
+          <th>Download file</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td>
+            <button>Download</button>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default Upload;
