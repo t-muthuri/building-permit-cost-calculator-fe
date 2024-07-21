7b52033ac14b4b3734b138d72b078d4d0c89cbed
t-muthuri <tressiemuthuri@gmail.com>
2024-07-21 18:27:22 +0300

    chore: about page edit

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -4,6 +4,18 @@ const About = () => {
   return (
     <div>
       <h1>What we are all about</h1>
+      <p>
+        {' '}
+        Making your architectural and construction endeavors as smooth and
+        informed as possible Whether you&apos;re embarking on a new building
+        project or staying updated with the latest industry news, we are here to
+        guide you every step of the way. We hope you find our resources
+        invaluable in your journey. Whether you&apos;re calculating the cost of
+        acquiring a building permit or seeking the latest news in construction
+        and architecture, we are committed to supporting you with accurate,
+        up-to-date information presented in a brief, summarized manner. Thank
+        you for choosing us.
+      </p>
     </div>
   );
 };
