e97b86811ef2576b1e7ccd0330631af7b435eb12
t-muthuri <tressiemuthuri@gmail.com>
2026-02-07 02:05:23 +0300

    fix: linting errors

--- .eslintrc.jsx
+++ .eslintrc.jsx
@@ -15,21 +15,29 @@ module.exports = {
     },
   },
   env: {
-    jest: true,
+    es2021: true,
     browser: true,
-    amd: true,
     node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:prettier/recommended', // this last element overrides all other formatting rules
+    'plugin:react/jsx-runtime', //no need to import React
   ],
+  globals: {
+    describe: 'readonly',
+    test: 'readonly',
+    expect: 'readonly',
+    vi: 'readonly',
+    beforeEach: 'readonly',
+    afterEach: 'readonly',
+  },
   rules: {
     'no-unused-vars': [
       'error',
       { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
     ],
-    'prettier/prettier': ['error', {}, { userPrettierrc: true }],
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
   },
 };
